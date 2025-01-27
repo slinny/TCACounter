37d60929c5ba3fb82e6998282c724e072d0d30b7
Siran Li <siranli411@gmail.com>
2025-01-27 14:06:06 -0600

    Initial Commit

---
+++ TCACounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCACounter
+//
+//  Created by Siran Li on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCACounter/TCACounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterApp.swift
+//  TCACounter
+//
+//  Created by Siran Li on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TCACounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
