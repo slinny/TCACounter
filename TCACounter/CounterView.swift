e8d5f649bb0d440b737ed46e5b5daa5ff0fc741d
Siran Li <siranli411@gmail.com>
2025-01-28 10:51:10 -0600

    update ContentView to CounterView and add Store to the App file and CounterView Preview

--- TCACounter/CounterView.swift
+++ TCACounter/CounterView.swift
@@ -6,19 +6,44 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct CounterView: View {
+    
+    let store: StoreOf<CounterFeature>
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("0")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    CounterView()
+    CounterView(
+        store: Store (initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+    )
 }
--- TCACounter/TCACounterApp.swift
+++ TCACounter/TCACounterApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCACounterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(
+                store: Store (initialState: CounterFeature.State()) {
+                    CounterFeature()
+                        ._printChanges()
+                }
+            )
         }
     }
 }
