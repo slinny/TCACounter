2d27d413f36c03b85f2c4c7935d26a6482b0676b
Siran Li <siranli411@gmail.com>
2025-01-29 21:59:07 -0600

    fix error in testGetFact func

--- TCACounterTests/TCACounterTests.swift
+++ TCACounterTests/TCACounterTests.swift
@@ -17,16 +17,16 @@ struct TCACounterTests {
         let store = TestStore(initialState: CounterFeature.State()) {
             CounterFeature()
         } withDependencies: {
-            $0.numberFact.fetch = { "\(0) is a great number" }
+            $0.numberFact.fetch = { "\($0) is a great number" }
         }
         
         await store.send(.factButtonTapped) {
             $0.isLoading = true
         }
         
-        await store.receive(.factResponse("\(0) is a great number")) {
+        await store.receive(\.factResponse) {
             $0.isLoading = false
-            $0.fact = "\(0) is a great number"
+            $0.fact = "0 is a great number"
         }
     }
     
